ba822b604ecf9eb262da1505e65e9fa099168754
JMichell159 <joemitchell159@gmail.com>
2025-10-20 21:02:00 -0500

    main now can be given a '--verbose' argument

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 import sys
 
 load_dotenv()
@@ -8,12 +9,23 @@ api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 
+verbose = False
+
 args = sys.argv
 if len(args) < 2:
     sys.exit("Error: A prompt must be provided")
 
-response = client.models.generate_content(model="gemini-2.0-flash-001", contents=args[1])
+if "--verbose" in args:
+    verbose = True
+    args.remove("--verbose")
+
+messages = [types.Content(role="user", parts=[types.Part(text=args[1])]),]
+
+response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages,)
+
+if verbose:
+    print(f"User prompt: {args[1]}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
