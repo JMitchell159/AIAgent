ac5807b78ad513c1594663646c2e7a497a064be9
JMichell159 <joemitchell159@gmail.com>
2025-10-22 06:43:37 -0500

    added get_file_content function that returns the characters of a file, up to a certain point

---
+++ functions/get_file_content.py
@@ -0,0 +1,19 @@
+import os
+from config import CHARACTER_LIMIT
+
+def get_file_content(working_directory, file_path):
+    full_path = os.path.join(working_directory, file_path)
+    result = ""
+    try:
+        if os.path.commonpath([os.path.abspath(working_directory)]) != os.path.commonpath([os.path.abspath(working_directory), os.path.abspath(full_path)]):
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        if os.path.isdir(full_path):
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+        large = os.path.getsize(full_path) > CHARACTER_LIMIT
+        with open(full_path, "r") as f:
+            result += f.read(CHARACTER_LIMIT)
+        if large:
+            result += f'[...File "{file_path}" truncated at {CHARACTER_LIMIT} characters]'
+        return result
+    except Exception as e:
+        return f'Error: {str(e)}'
