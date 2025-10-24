646f15a800c6056f6bc1e651a1c9647a6f93dc7b
JMichell159 <joemitchell159@gmail.com>
2025-10-24 06:58:24 -0500

    ai can now generate an answer from code or fix simple coding errors

--- call_function.py
+++ call_function.py
@@ -1,33 +1,45 @@
-from functions.get_file_content import get_file_content
-from functions.get_files_info import get_files_info
-from functions.run_python_file import run_python_file
-from functions.write_file import write_file
+from functions.get_file_content import get_file_content, schema_get_file_content
+from functions.get_files_info import get_files_info, schema_get_files_info
+from functions.run_python_file import run_python_file, schema_run_python_file
+from functions.write_file import write_file, schema_write_file
 from google import genai
 from google.genai import types
 
-call_dict = {"get_file_content": get_file_content, "get_files_info": get_files_info, "run_python_file": run_python_file, "write_file": write_file}
+available_functions = types.Tool(function_declarations=[schema_get_files_info, schema_get_file_content, schema_run_python_file, schema_write_file,])
 
-def call_function(call, verbose=False):
+def call_function(function_call_part, verbose=False):
     if verbose:
-        print(f"Calling function: {call.name}({call.args})")
+        print(
+            f" - Calling function: {function_call_part.name}({function_call_part.args})"
+        )
     else:
-        print(f" - Calling function: {call.name}")
-    if call.name in call_dict:
+        print(f" - Calling function: {function_call_part.name}")
+    function_map = {
+        "get_files_info": get_files_info,
+        "get_file_content": get_file_content,
+        "run_python_file": run_python_file,
+        "write_file": write_file,
+    }
+    function_name = function_call_part.name
+    if function_name not in function_map:
         return types.Content(
             role="tool",
             parts=[
                 types.Part.from_function_response(
-                    name=call.name,
-                    response={"result": call_dict[call.name](working_directory="./calculator", **call.args)},
+                    name=function_name,
+                    response={"error": f"Unknown function: {function_name}"},
                 )
             ],
         )
+    args = dict(function_call_part.args)
+    args["working_directory"] = "./calculator"
+    function_result = function_map[function_name](**args)
     return types.Content(
         role="tool",
         parts=[
             types.Part.from_function_response(
-                name=call.name,
-                response={"error": f"Unknown function: {call.name}"},
+                name=function_name,
+                response={"result": function_result},
             )
         ],
     )
